a7134b1bc9fad55ef1eea2c3a85b875f3c4e178c
Erin Orstrom <eorstrom2010@gmail.com>
2019-07-17 22:48:09 -0500

    Add project files.

---
+++ ComicBookGalleryModel/Models/Series.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class Series
+    {
+        public Series()
+        {
+            ComicBook = new List<ComicBook>();
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string MyProperty { get; set; }
+
+        public ICollection<ComicBook> ComicBooks { get; set; }
+        public List<ComicBook> ComicBook { get; private set; }
+    }
+}
